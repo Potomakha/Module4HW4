73d0364735742e6868ab7deb7d0420a1984b0ec0
Oleh Potomakha <potomakha@gmail.com>
2022-01-21 19:53:19 +0200

    Add project files.

---
+++ Module4HW4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module4HW4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
